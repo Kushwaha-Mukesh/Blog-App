ac901fb5af87ab3a79afc2151442fb5c8cb9ad42
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-04-07 18:21:11 +0545

    added responsive header

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -4,16 +4,20 @@ import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/Sign-Up";
 import SignIn from "./pages/Sign-In";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/sign-in" element={<SignUp />} />
-        <Route path="/sign-up" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ blog-frontend/src/components/Header.jsx
@@ -0,0 +1,85 @@
+import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import { IoIosMoon } from "react-icons/io";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    setShow(!show);
+  };
+  return (
+    <>
+      <div className="flex align-middle justify-between p-2">
+        <Link to={"/"}>
+          <p className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg p-1 text-white px-2">
+            Mukesh's <span>Blog</span>
+          </p>
+        </Link>
+        <form className="border border-black py-1 px-2 rounded-lg">
+          <input
+            type="text"
+            name="search"
+            id=""
+            className="mr-1 focus:outline-none bg-transparent"
+          />
+          <button type="submit">
+            <BsSearch />
+          </button>
+        </form>
+        <div className="md:flex md:space-x-16 hidden">
+          <Link to={"/"}>
+            <span>Home</span>
+          </Link>
+          <Link to={"/about"}>
+            <span>About</span>
+          </Link>
+          <Link to={"/projects"}>
+            <span>Projects</span>
+          </Link>
+        </div>
+        <div className="sm:flex justify-center hidden">
+          <span className="text-lg bg-black text-white rounded-lg px-2">
+            <IoIosMoon className="relative top-2" />
+          </span>
+          <Link
+            to={"/sign-in"}
+            className="border border-black rounded-lg px-2 ml-2"
+          >
+            Sign in
+          </Link>
+        </div>
+        <div className="md:hidden cursor-pointer" onClick={handleClick}>
+          <GiHamburgerMenu className="text-[30px]" />
+        </div>
+      </div>
+
+      {show && (
+        <div className="flex flex-col justify-between h-[80vh] absolute bg-white w-[200px] top-12 right-0 rounded-lg p-4">
+          <div className="flex flex-col gap-4">
+            <Link>
+              <span>Home</span>
+            </Link>
+            <Link>
+              <span>About</span>
+            </Link>
+            <Link>
+              <span>Projects</span>
+            </Link>
+          </div>
+          <div className="w-full">
+            <span className="text-lg w-full rounded-lg px-2">
+              <IoIosMoon />
+            </span>
+            <Link>
+              <button>Sign in</button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
